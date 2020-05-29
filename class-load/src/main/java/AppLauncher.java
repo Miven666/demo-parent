4de383ea3fb7b57b4728458e7b831e0b50a6fcdf
mingzhi.xie <xie6032mail@163.com>
2020-05-29 16:15:49 +0800

    新增 ClassLoad 模块

---
+++ class-load/src/main/java/AppLauncher.java
@@ -0,0 +1,10 @@
+/**
+ * @author mingzhi.xie
+ * @date 2020/3/16
+ * @since 1.0
+ */
+public class AppLauncher {
+    public static void main(String[] args) {
+        System.out.println("App launcher");
+    }
+}
